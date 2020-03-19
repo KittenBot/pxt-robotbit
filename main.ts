93a2a255e480798940d21ba7a32dfd125aad16e2
hogan96 <234513426@qq.com>
2020-03-19 10:26:00 +0800

    transTW

--- main.ts
+++ main.ts
@@ -495,21 +495,10 @@ namespace robotbit {
         matrixShow();
     }
 
-    //% blockId=robotbit_ultrasonic block="Ultrasonic|pin %pin"
+    //% blockId=robotbit_rgbultrasonic block="Ultrasonic|pin %pin"
     //% weight=10
-    export function Ultrasonic(pin: DigitalPin): number {
-        return UltrasonicVer(pin, SonarVersion.V1);
-    }
-
-    //% blockId=robotbit_ultrasonicver block="Ultrasonic|pin %pin|version %v"
-    //% weight=10
-    export function UltrasonicVer(pin: DigitalPin, v: SonarVersion): number {
-
-        // send pulse
-        if (v == SonarVersion.V1) {
-            pins.setPull(pin, PinPullMode.PullNone);
-        }
-        else { pins.setPull(pin, PinPullMode.PullDown); }
+    export function RgbUltrasonic(pin: DigitalPin): number {
+        pins.setPull(pin, PinPullMode.PullNone);
         pins.digitalWritePin(pin, 0);
         control.waitMicros(2);
         pins.digitalWritePin(pin, 1);
@@ -523,12 +512,34 @@ namespace robotbit {
         if (ret == 0 && distanceBuf != 0) {
             ret = distanceBuf;
         }
-        distanceBuf = d;
-        if (v == SonarVersion.V1) {
-            return Math.floor(ret * 9 / 6 / 58);
+        distanceBuf = d;   
+
+        return Math.floor(ret * 9 / 6 / 58);
+    }
+
+    //% blockId=robotbit_holeultrasonicver block="Ultrasonic|pin %pin|version %v"
+    //% weight=10
+    export function HoleUltrasonic(pin: DigitalPin): number {
+
+        // send pulse
+        pins.setPull(pin, PinPullMode.PullDown);
+        pins.digitalWritePin(pin, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(pin, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(pin, 0);
+
+        // read pulse
+        let d = pins.pulseIn(pin, PulseValue.High, 25000);
+        let ret = d;
+        // filter timeout spikes
+        if (ret == 0 && distanceBuf != 0) {
+            ret = distanceBuf;
         }
+
+        distanceBuf = d;
+
         return Math.floor(ret / 40 + (ret / 800));
-        // Correction
     }
 
 }
