2198e1e057d399e6b558597d40a73e02dbfdf077
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-18 07:46:16 -0700

    Update jacdac/pxt.json, jacdac/main.ts, jacdac/test.ts

--- jacdac/main.ts
+++ jacdac/main.ts
@@ -7,23 +7,56 @@ namespace modules {
     /**
      * Motor 1A
      */
-    //% fixedInstance whenUsed block="robobit motor 1A"
-    export const robobitMotor1A = new MotorClient("robobit motor 1A?device=self")
+    //% fixedInstance whenUsed block="robotbit motor 1A"
+    export const robotbitMotor1A = new MotorClient("robotbit motor 1A?device=self")
     /**
      * Motor 1B
      */
-    //% fixedInstance whenUsed block="robobit motor 1B"
-    export const robobitMotor1B = new MotorClient("robobit motor 1B?device=self")
+    //% fixedInstance whenUsed block="robotbit motor 1B"
+    export const robotbitMotor1B = new MotorClient("robotbit motor 1B?device=self")
     /**
      * Motor 2A
      */
-    //% fixedInstance whenUsed block="robobit motor 2A"
-    export const robobitMotor2A = new MotorClient("robobit motor 2A?device=self")
+    //% fixedInstance whenUsed block="robotbit motor 2A"
+    export const robotbitMotor2A = new MotorClient("robotbit motor 2A?device=self")
     /**
      * Motor 2B
      */
-    //% fixedInstance whenUsed block="robobit motor 2B"
-    export const robobitMotor2B = new MotorClient("robobit motor 2B?device=self")
+    //% fixedInstance whenUsed block="robotbit motor 2B"
+    export const robotbitMotor2B = new MotorClient("robotbit motor 2B?device=self")
+
+    /**
+     * Servo S1
+     */
+    export const robotbitServoS1 = new ServoClient("robotbit servo S1?device=self")
+    /**
+     * Servo S2
+     */
+    export const robotbitServoS2 = new ServoClient("robotbit servo S2?device=self")
+    /**
+     * Servo S3
+     */
+    export const robotbitServoS3 = new ServoClient("robotbit servo S3?device=self")
+    /**
+     * Servo S4
+     */
+    export const robotbitServoS4 = new ServoClient("robotbit servo S4?device=self")
+    /**
+     * Servo S5
+     */
+    export const robotbitServoS5 = new ServoClient("robotbit servo S5?device=self")
+    /**
+     * Servo S6
+     */
+    export const robotbitServoS6 = new ServoClient("robotbit servo S6?device=self")
+    /**
+     * Servo S7
+     */
+    export const robotbitServoS7 = new ServoClient("robotbit servo S7?device=self")
+    /**
+     * Servo S8
+     */
+    export const robotbitServoS8 = new ServoClient("robotbit servo S8?device=self")
 }
 
 namespace servers {
@@ -33,8 +66,8 @@ namespace servers {
         speed: number
         enabled: boolean
 
-        constructor(motor: robotbit.Motors) {
-            super(jacdac.SRV_MOTOR)
+        constructor(motor: robotbit.Motors, instanceName: string) {
+            super(jacdac.SRV_MOTOR, { instanceName: instanceName })
             this.motor = motor
             this.enabled = false
 
@@ -57,12 +90,37 @@ namespace servers {
             }
         }
     }
+    class ServoServer extends jacdac.Server {
+        servo: robotbit.Servos
+        angle: number
+        constructor(servo: robotbit.Servos, instanceName: string) {
+            super(jacdac.SRV_SERVO, { instanceName: instanceName })
+            this.servo = servo
+            this.angle = 0
+        }
+        handlePacket(pkt: jacdac.JDPacket) {
+            this.angle = this.handleRegValue(pkt, jacdac.ServoReg.Angle, jacdac.ServoRegPack.Angle, this.angle)
+            this.handleRegValue(pkt, jacdac.ServoReg.CurrentAngle, jacdac.ServoRegPack.CurrentAngle, this.angle)
+
+            robotbit.Servo(this.servo, this.angle)
+        }
+    }
     function start() {
         jacdac.startSelfServers(() => [
-            new MotorServer(robotbit.Motors.M1A),
-            new MotorServer(robotbit.Motors.M1B),
-            new MotorServer(robotbit.Motors.M2A),
-            new MotorServer(robotbit.Motors.M2B),
+            // motors
+            new MotorServer(robotbit.Motors.M1A, "M1A"),
+            new MotorServer(robotbit.Motors.M1B, "M1B"),
+            new MotorServer(robotbit.Motors.M2A, "M2A"),
+            new MotorServer(robotbit.Motors.M2B, "M2B"),
+            // servos
+            new ServoServer(robotbit.Servos.S1, "S1"),
+            new ServoServer(robotbit.Servos.S2, "S2"),
+            new ServoServer(robotbit.Servos.S3, "S3"),
+            new ServoServer(robotbit.Servos.S4, "S4"),
+            new ServoServer(robotbit.Servos.S5, "S5"),
+            new ServoServer(robotbit.Servos.S6, "S6"),
+            new ServoServer(robotbit.Servos.S7, "S7"),
+            new ServoServer(robotbit.Servos.S8, "S8"),
         ])
     }
     start()
--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,15 +1,21 @@
 let speed = 0
 let dv = 5
 forever(() => {
-    modules.robobitMotor1A.setEnabled(true)
-    modules.robobitMotor1B.setEnabled(true)
-    modules.robobitMotor2A.setEnabled(true)
-    modules.robobitMotor2B.setEnabled(true)
+    led.toggle(0, 0)
+    
+    modules.robotbitMotor1A.run(speed)
+    modules.robotbitMotor1B.run(100 - speed)
+    modules.robotbitMotor2A.run(speed / 2)
+    modules.robotbitMotor2B.run((100 - speed) / 2)
 
-    modules.robobitMotor1A.setDuty(speed)
-    modules.robobitMotor1B.setDuty(1 - speed)
-    modules.robobitMotor2A.setDuty(speed / 2)
-    modules.robobitMotor2B.setDuty((1 - speed) / 2)
+    modules.robotbitServoS1.setAngle(speed)
+    modules.robotbitServoS2.setAngle(speed)
+    modules.robotbitServoS3.setAngle(speed)
+    modules.robotbitServoS4.setAngle(speed)
+    modules.robotbitServoS5.setAngle(speed)
+    modules.robotbitServoS6.setAngle(speed)
+    modules.robotbitServoS7.setAngle(speed)
+    modules.robotbitServoS8.setAngle(speed)
 
     speed += dv
     if (speed > 100) {
