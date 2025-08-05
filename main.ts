eb145b17b55769d3fcef71f4b20cf43d05482538
zopendoor <1556696929@qq.com>
2025-08-05 11:26:38 +0800

    修复5kg舵机电机模式下速度设置为0 舵机不停止问题

--- main.ts
+++ main.ts
@@ -309,19 +309,37 @@ namespace robotbit {
     //% group="Servo" weight=58
     //% speed.min=-255 speed.max=255
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function GeekServo5KG_Motor(index: Servos, speed: number): void { //5KG的电机模式 3000-5000 4000是回中
+    export function GeekServo5KG_Motor(index: Servos, speed: number): void {
         if (!initialized) {
-            initPCA9685()
+            initPCA9685();
+        }
+        if (speed === 0) {
+            setPwm(index + 7, 0, 843); // 直接硬编码
+            return;
         }
         const minInput = -255;
         const maxInput = 255;
         const minOutput = 5000;
         const maxOutput = 3000;
-
         const v_us = ((speed - minInput) / (maxInput - minInput)) * (maxOutput - minOutput) + minOutput;
-        let value = v_us * 4096 / 20000
-        setPwm(index + 7, 0, value)
-    }	
+        const value = Math.round(v_us * 4096 / 20000);
+        setPwm(index + 7, 0, value);
+    }
+    //export function GeekServo5KG_Motor(index: Servos, speed: number): void { //5KG的电机模式 3000-5000 4000是回中
+      //  if (!initialized) {
+            //initPCA9685()
+        //}
+        //const minInput = -255;
+        //const maxInput = 255;
+        //const minOutput = 5000;
+        //const maxOutput = 3000;
+
+        //const v_us = ((speed - minInput) / (maxInput - minInput)) * (maxOutput - minOutput) + minOutput;
+        //let value = v_us * 4096 / 20000
+        //setPwm(index + 7, 0, value)
+    //}
+
+
 	
 
     //% blockId=robotbit_stepper_degree block="Stepper 28BYJ-48|%index|degree %degree"
